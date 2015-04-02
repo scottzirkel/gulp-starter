86e73476bf55303c548e943e86e9a8d27b883524
Scott Zirkel <scott@scottzirkel.com>
2015-04-02 16:36:24 -0500

    Fixed build issues

--- shipitconfig.js
+++ shipitconfig.js
@@ -1,23 +1,9 @@
-// module.exports = {
-//   projectName: 'PROJECT_NAME',
-//   repo: 'HTTPS REPO URL',
-//   staging: {
-//     servers: 'USERNAME@SERVER',
-//     deployTo: '/absolute/path/to/www/root'
-//   },
-//   production: {
-//     servers: 'USERNAME@SERVER',
-//     deployTo: '/absolute/path/to/www/root'
-//   }
-// };
-
-
 module.exports = {
-  projectName: 'gulp-starter',
-  repo: 'https://github.com/scottzirkel/gulp-starter.git',
+  projectName: 'PROJECT_NAME',
+  repo: 'HTTPS REPO URL',
   staging: {
-    servers: 'tagnetdev@70.32.76.228',
-    deployTo: '/var/vhosts/www/alaradevelopment/gulp-starter.alaradevlopment.com'
+    servers: 'USERNAME@SERVER',
+    deployTo: '/absolute/path/to/www/root'
   },
   production: {
     servers: 'USERNAME@SERVER',
