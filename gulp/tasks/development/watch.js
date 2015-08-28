d8e8a0ca4776ce3a9fda0d7af3e6718c9bc56191
Scott Zirkel <scott@scottzirkel.com>
2015-08-28 14:49:33 -0500

    Updated watch & header

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -2,7 +2,7 @@ var gulp    = require('gulp'),
     config  = require('../../config').watch;
 
 gulp.task('watch', ['browsersync'], function () {
-  gulp.watch(config.sass, ['compass']);
+  gulp.watch(config.sass, ['sass']);
   gulp.watch(config.html, ['html']);
   gulp.watch(config.images, ['images']);
 });
